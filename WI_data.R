6d145acb667593a764fc7a674431a2b7df971414
Cudd1eB34R <jling825@gmail.com>
2018-05-08 23:56:37 -0700

    Update WI_data.R

--- WI_data.R
+++ WI_data.R
@@ -2,6 +2,7 @@
 library(forecast)
 library(stats)
 library(stargazer)
+library(vars)
 
 #### Lyme disease Data ####
 WI09 <- c(16, 23, 26, 36, 115, 464, 647, 286, 148, 82, 60, 45)
@@ -12,6 +13,10 @@ WI13 <- c(20, 11, 14, 13, 109, 398, 594, 284, 167, 71, 28, 17)
 WI14 <- c(16, 9, 15, 27, 85, 254, 320, 115, 78, 45, 11, 9)
 WI15 <- c(13, 8, 11, 33, 144, 376, 373, 142, 77, 74, 28, 22)
 WI16 <- c(9, 14, 28, 57, 127, 433, 438, 146, 80, 91, 47, 21)
+
+WI <- c(WI09, WI10, WI11, WI12, WI13, WI14, WI15, WI16)
+
+WI2 <- WI / 8.32
 
 # Year and Month
 Year <- rep(x = c(2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016),
@@ -25,20 +30,17 @@ trend <- origin[61:156,]
 
 #### Master List ####
 # master set
-master <- data.frame(cbind(trend,
-                           c(WI09, WI10, WI11, WI12, WI13, WI14, WI15, WI16)))
-names(master) = c("Date", "Trend", "Reports")
+master <- data.frame(cbind(trend, WI, WI2))
+names(master) = c("Trend", "Reports", "W.Reports")
 
 # declaring time series data
-lyme <- ts(master$Reports,
+lyme <- ts(master$W.Reports,
            start = c(2009, 1),
            frequency = 12)
 
-lyme2 <- lyme / 8.32
-
-trend <- ts(master$Trend,
-            start = c(2009, 1),
-            frequency = 12)
+master <- ts(master[-2],
+             start = c(2009, 1),
+             frequency = 12)
 
 # initial plots
 plot(lyme2)
